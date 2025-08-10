64aa379e71c7e5b7e196f942623e5e164d5c7a0e
Sait <said-bah@yandex.ru>
2025-08-10 12:20:32 +0300

    Переименовал в метод submitData

--- fstr_app/urls.py
+++ fstr_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import PerevalCreateView
 
 urlpatterns = [
-    path('pereval/', PerevalCreateView.as_view(), name='pereval-create'),
+    path('submitData/', PerevalCreateView.as_view(), name='submitData'),
 ]
